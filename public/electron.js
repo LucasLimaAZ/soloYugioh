d0f2732b5a22ff588b9a2c3ae131ebf280b3085f
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2022-05-03 19:46:45 -0300

    electron

---
+++ public/electron.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+
+require("@electron/remote/main").initialize();
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      enableRemoteModule: true,
+    },
+  });
+
+  win.loadURL("http://localhost:3006");
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
